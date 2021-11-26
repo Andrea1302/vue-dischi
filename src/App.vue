25f8c4bcafa60b2839f4b13cdb322e2f2179d8b1
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-11-26 13:01:33 +0100

    Start page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <i class="fab fa-spotify logo"></i>
+    </header>
+
+    <main>
+      <!-- qui ci sarà il componente  -->
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import fontawesome from "fontawesome"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style lang="scss">
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+header{
+  background-color: rgb(59, 58, 58);
+  padding: 10px 5px;
+}
+.logo{
+  color: #1ed760;
+  font-size: 40px;
+  background-color: black;
+  border-radius: 50%;
 }
 </style>
